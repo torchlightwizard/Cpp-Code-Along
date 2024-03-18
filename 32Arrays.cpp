9ba2a82439d78e7c5e090dc934498fbd55537e36
Astro <rustruedetective@gmail.com>
2024-03-18 22:44:41 +0500

    32

---
+++ 32Arrays.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(void){
+    int ar[5] = {1,2,3,4,5};   //array with initialization list
+    std::cout << ar[0] << std::endl;
+    std::cout << ar[1] << std::endl;
+    std::cout << ar[2] << std::endl;
+    std::cout << ar[3] << std::endl;
+    std::cout << ar[4] << std::endl;
+
+    return 0;
+}
